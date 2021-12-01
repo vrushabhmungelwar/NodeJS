91f81f5c5c458da38bdf299c31d532fc2b3d620b
vrushabhmungelwar <vrushabhmungelwar10@gmail.com>
2021-12-01 16:39:12 +0530

    put delete and dotenv

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // const express = require("express");  //  "type": "common.js",
 import express from "express"; //   "type": "module",
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env)
 
 const app = express();
 
@@ -8,81 +12,10 @@ const PORT = 9000;
 
 app.use(express.json());
 
-// const movies = [
-//   {
-//     id: "100",
-//     name: "Iron man 2",
-//     poster:
-//       "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-//     rating: 7,
-//     summary:
-//       "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-//     trailer: "https://www.youtube.com/embed/wKtcmiifycU",
-//     language: "english",
-//   },
-//   {
-//     id: "101",
-//     name: "No Country for Old Men",
-//     poster:
-//       "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-//     rating: 8.1,
-//     summary:
-//       "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-//     trailer: "https://www.youtube.com/embed/38A__WT3-o0",
-//   },
-//   {
-//     id: "102",
-//     name: "Jai Bhim",
-//     poster:
-//       "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-//     summary:
-//       "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-//     rating: 8.8,
-//     trailer: "https://www.youtube.com/embed/nnXpbTFrqXA",
-//     language: "english",
-//   },
-//   {
-//     id: "103",
-//     name: "The Avengers",
-//     rating: 8,
-//     summary:
-//       "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-//     poster:
-//       "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-//     trailer: "https://www.youtube.com/embed/eOrNdBpGMv8",
-//   },
-//   {
-//     id: "104",
-//     name: "Interstellar",
-//     poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-//     rating: 8.6,
-//     summary:
-//       "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-//     trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
-//   },
-//   {
-//     id: "105",
-//     name: "Baahubali",
-//     poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-//     rating: 8,
-//     summary:
-//       "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-//     trailer: "https://www.youtube.com/embed/sOEg_YZQsTI",
-//   },
-//   {
-//     id: "106",
-//     name: "Ratatouille",
-//     poster:
-//       "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-//     rating: 8,
-//     summary:
-//       "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-//     trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w",
-//     language: "english",
-//   },
-// ];
-
-const MONGO_URL = "mongodb://localhost";
+// const MONGO_URL = "mongodb://localhost";
+const MONGO_URL =  process.env.MONGO_URL;
+//   "mongodb+srv://vrushabh:welcome123@cluster0.zksv2.mongodb.net";
+// mongodb+srv://vrushabh:<password>@cluster0.zksv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
@@ -101,7 +34,7 @@ app.get("/movies", async (request, response) => {
   const filter = request.query;
   console.log(filter);
   if (filter.rating) {
-    filter.rating = parseInt(filter.rating);
+    filter.rating = +filter.rating;
   }
   // const { language, rating } = request.query;
   // const {rating} = request.query;
@@ -115,23 +48,15 @@ app.get("/movies", async (request, response) => {
   // if (rating) {
   //   filterMovies = filterMovies.filter((mv) => mv.rating === +rating);
   // }
-  const filterMovies = await client
-    .db("movies")
-    .collection("movies")
-    .find(filter)
-    // .limit(2)
-    .toArray();
-  console.log(filterMovies);
+  const filterMovies = await getMovies(filter);
+  // console.log(filterMovies);
   response.send(filterMovies);
 });
 
 app.post("/movies", async (request, response) => {
   const data = request.body;
   // console.log(data);
-  const result = await client
-    .db("movies")
-    .collection("movies")
-    .insertMany(data);
+  const result = await createMovies(data);
   response.send(result);
 });
 
@@ -139,16 +64,60 @@ app.get("/movies/:id", async (request, response) => {
   console.log(request.params);
   const { id } = request.params;
 
-  const movie = await client
-    .db("movies")
-    .collection("movies")
-    .findOne({ id: id });
+  const movie = await getMoviebyId(id);
 
   // const movie = movies.find((mv) => mv.id === id);
   console.log(movie);
   movie
     ? response.send(movie)
-    : response.status(404).send({ message: "no matching movie" });
+    : response.status(404).send({ message: "no matching movie found" });
+});
+
+app.delete("/movies/:id", async (request, response) => {
+  console.log(request.params);
+  const { id } = request.params;
+
+  const result = await deleteMovieById(id);
+
+  result.deletedCount > 0
+    ? response.send(result)
+    : response.status(404).send({ message: "no matching movie found" });
+});
+
+app.put("/movies/:id", async (request, response) => {
+  console.log(request.params);
+  const { id } = request.params;
+  const data = request.body;
+  const result = await updateMovieById(id, data);
+const movie = await getMoviebyId(id);
+  response.send(movie);
 });
 
 app.listen(PORT, () => console.log("App is started", PORT));
+async function updateMovieById(id, data) {
+  return await client
+    .db("movies")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: data });
+}
+
+async function createMovies(data) {
+  return await client.db("movies").collection("movies").insertMany(data);
+}
+
+async function getMovies(filter) {
+  return await client
+    .db("movies")
+    .collection("movies")
+    .find(filter)
+    // .limit(2)
+    .toArray();
+}
+
+async function deleteMovieById(id) {
+  return await client.db("movies").collection("movies").deleteOne({ id: id });
+}
+
+async function getMoviebyId(id) {
+  return await client.db("movies").collection("movies").findOne({ id: id });
+}
